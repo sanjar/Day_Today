ea5fef096b5f8f03c46d8271c71905b3f580f6d9
Sadique <sanjar.sadique@gmail.com>
2017-06-03 13:37:59 +0530

    Toptal Test

---
+++ TotalCodeTest/src/com/toptal/test/ChessMove.java
@@ -0,0 +1,16 @@
+package com.toptal.test;
+
+public class ChessMove {
+	public static void main(String[] args) {
+		ChessMove chessMove = new ChessMove();
+		int A = 4;
+		int B = 5;
+		System.out.println(chessMove.solution(A, B));
+	}
+
+	public int solution(int A, int B) {
+
+		return B;
+		// write your code in Java SE 8
+	}
+}
