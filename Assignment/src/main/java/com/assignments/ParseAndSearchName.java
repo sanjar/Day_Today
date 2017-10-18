80da8f57d66541232edc86cbdae658c451a8491a
Mohammad Sadique Sanjar <mohammad.sanjar@daimler.com>
2017-10-18 23:17:55 +0530

    java8

--- Assignment/src/main/java/com/assignments/ParseAndSearchName.java
+++ Assignment/src/main/java/com/assignments/ParseAndSearchName.java
@@ -9,6 +9,43 @@ import java.util.Scanner;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+/**
+ * 
+ * @author MSANIAR
+ *
+ *
+ *Text le Description
+The text les boynames.txt and girlnames.txt, which are included in the source code
+for this book text, contain a list of the 1,000 most popular boy and girl names in the United
+States for the year 2003 as compiled by the Social Security Administration.
+These are blank-delimited les, where the most popular name is listed rst, the second
+most popular name is listed second, and so on, to the 1,000th most popular name, which
+is listed last.
+Each line consists of the rst name followed by a blank space and then the number of
+registered births using that name in the year. For example, the girlnames.txt le begins
+with :
+ Emily 25494
+ Emma 22532
+ Madison 19986
+This indicates that Emily was the most popular name with 25,494 registered namings,
+Emma was the second most popular with 22,532, and Madison was the third most popular
+with 19,986.
+2 Project Requirements.
+ Write a program that reads both the girl and boy les into memory using arrays.
+ Then, allow the user to input a name.
+ The program should search through both arrays.
+1
+ If there is a match, then it should output the popularity ranking and the number of
+namings.
+ The program should also indicate if there is no match.
+ For example, if the user enters the name \Justice", then the program should output:
+Justice is ranked 456 in popularity among girls with 655 namings.
+Justice is ranked 401 in popularity among boys with 653 namings.
+ If the user enters the name \Walter", then the program should output:
+Walter is not ranked among the top 1000 girl names.
+Walter is ranked 356 in popularity among boys with 775 namings.
+
+ */
 public class ParseAndSearchName {
 	private static int NUMNAME = 1000;
 	private static String[] boyNames = new String[NUMNAME];
